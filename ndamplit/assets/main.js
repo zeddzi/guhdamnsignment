d12d600cbd2aedb3be61b07c7209f5f0967a508d
zeddzi <i.am.mbuguaa34@gmail.com>
2022-06-21 20:12:01 +0300

    ndamlit

---
+++ ndamplit/assets/main.js
@@ -0,0 +1,7 @@
+const btn = document.createElement("button");
+btn.innerHTML = "Click me";
+btn.addEventListener("click", () =>
+{
+  alert("Hello Baraka, welcome to the bootcamp");
+});
+document.body.appendChild(btn);
